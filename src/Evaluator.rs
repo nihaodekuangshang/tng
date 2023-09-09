23c8e8e988c8827d90b2caf97ad4c1dd2a54709e
Cron <beamingadam@gmail.com>
2023-09-09 16:51:55 +0800

    更新

--- src/Evaluator.rs
+++ src/Evaluator.rs
@@ -3,29 +3,47 @@ pub fn eval(ex: &Expression) {
     match ex {
         Expression::Num(num) => print!("push {}\n", num),
         Expression::Cons(op, V) => {
-            eval(V.get(0).unwrap());
-            eval(V.get(1).unwrap());
-            print!("    pop rdi\n");
-            print!("    pop rax\n");
-            match op {
-                '+' => {
-                    print!("    add rax,rdi\n");
-                    print!("    push rax\n");
+            if V.len() < 2 {
+                print!("    push 0\n");
+                eval(V.get(0).unwrap());
+                print!("    pop rdi\n");
+                print!("    pop rax\n");
+                match op {
+                    '+' => {
+                        print!("    add rax,rdi\n");
+                        print!("    push rax\n");
+                    }
+                    '-' => {
+                        print!("    sub rax,rdi\n");
+                        print!("    push rax\n");
+                    }
+                    _ => (),
                 }
-                '-' => {
-                    print!("    sub rax,rdi\n");
-                    print!("    push rax\n");
+            } else {
+                eval(V.get(0).unwrap());
+                eval(V.get(1).unwrap());
+                print!("    pop rdi\n");
+                print!("    pop rax\n");
+                match op {
+                    '+' => {
+                        print!("    add rax,rdi\n");
+                        print!("    push rax\n");
+                    }
+                    '-' => {
+                        print!("    sub rax,rdi\n");
+                        print!("    push rax\n");
+                    }
+                    '*' => {
+                        print!("    imul rax,rdi\n");
+                        print!("    push rax\n");
+                    }
+                    '/' => {
+                        print!("    cqo\n");
+                        print!("    idiv rdi\n");
+                        print!("    push rax\n");
+                    }
+                    _ => (),
                 }
-                '*' => {
-                    print!("    imul rax,rdi\n");
-                    print!("    push rax\n");
-                }
-                '/' => {
-                    print!("    cqo\n");
-                    print!("    idiv rdi\n");
-                    print!("    push rax\n");
-                }
-                _ => (),
             }
         }
     }
